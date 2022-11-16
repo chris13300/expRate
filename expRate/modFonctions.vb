5eee0f01f07fef921776d1ee29934e7649071ff3
Christophe JEAN <54830918+chris13300@users.noreply.github.com>
2022-11-16 11:01:00 +0100

    Add files via upload

---
+++ expRate/modFonctions.vb
@@ -0,0 +1,300 @@
+﻿Imports VB = Microsoft.VisualBasic
+
+Module modFonctions
+    Public processus As System.Diagnostics.Process
+    Public entree As System.IO.StreamWriter
+    Public sortie As System.IO.StreamReader
+    Public moteur_court As String
+
+    Public Sub chargerMoteur(chemin As String, fichierEXP As String)
+        Dim chaine As String
+
+chargement_moteur:
+        Try
+            processus = New System.Diagnostics.Process()
+
+            processus.StartInfo.RedirectStandardOutput = True
+            processus.StartInfo.UseShellExecute = False
+            processus.StartInfo.RedirectStandardInput = True
+            processus.StartInfo.CreateNoWindow = True
+            processus.StartInfo.WorkingDirectory = My.Application.Info.DirectoryPath
+            processus.StartInfo.FileName = chemin
+            processus.Start()
+            processus.PriorityClass = 64 '64 (idle), 16384 (below normal), 32 (normal), 32768 (above normal), 128 (high), 256 (realtime)
+
+            entree = processus.StandardInput
+            sortie = processus.StandardOutput
+
+            entree.WriteLine("uci")
+            chaine = ""
+            While InStr(chaine, "uciok") = 0
+                If processus.HasExited Then
+                    entree.Close()
+                    sortie.Close()
+                    processus.Close()
+                    GoTo chargement_moteur
+                End If
+                chaine = sortie.ReadLine
+                Threading.Thread.Sleep(10)
+            End While
+
+            entree.WriteLine("setoption name threads value 1")
+            entree.WriteLine("setoption name Experience File value " & fichierEXP)
+
+            entete = ""
+            While entete = ""
+                If processus.HasExited Then
+                    entree.Close()
+                    sortie.Close()
+                    processus.Close()
+                    GoTo chargement_moteur
+                End If
+                chaine = sortie.ReadLine
+                If InStr(chaine, "info", CompareMethod.Text) > 0 _
+                And InStr(chaine, "string", CompareMethod.Text) > 0 _
+                And (InStr(chaine, "collision", CompareMethod.Text) > 0 Or InStr(chaine, "duplicate", CompareMethod.Text) > 0) Then
+                    entete = Replace(chaine, fichierEXP, nomFichier(fichierEXP)) & vbCrLf
+                End If
+                Threading.Thread.Sleep(10)
+            End While
+
+            entree.WriteLine("isready")
+            chaine = ""
+            While InStr(chaine, "readyok") = 0
+                If processus.HasExited Then
+                    entree.Close()
+                    sortie.Close()
+                    processus.Close()
+                    GoTo chargement_moteur
+                End If
+                chaine = sortie.ReadLine
+                Threading.Thread.Sleep(10)
+            End While
+        Catch ex As Exception
+            If processus.HasExited Then
+                entree.Close()
+                sortie.Close()
+                processus.Close()
+                GoTo chargement_moteur
+            End If
+        End Try
+
+    End Sub
+
+    Public Sub dechargerMoteur()
+        entree.Close()
+        sortie.Close()
+        processus.Close()
+
+        entree = Nothing
+        sortie = Nothing
+        processus = Nothing
+    End Sub
+
+    Public Function expListe(position As String) As String
+        Dim chaine As String, ligne As String
+
+        If position = "" Then
+            entree.WriteLine("position startpos")
+        ElseIf InStr(position, "/", CompareMethod.Text) > 0 _
+          And (InStr(position, " w ", CompareMethod.Text) > 0 Or InStr(position, " b ", CompareMethod.Text) > 0) Then
+            entree.WriteLine("position fen " & position)
+        Else
+            entree.WriteLine("position startpos moves " & position)
+        End If
+        entree.WriteLine("expex")
+
+        entree.WriteLine("isready")
+
+        chaine = ""
+        ligne = ""
+        While InStr(ligne, "readyok") = 0
+            ligne = sortie.ReadLine
+            If InStr(ligne, "Fen: ", CompareMethod.Text) > 0 Then
+                epd = Trim(Replace(ligne, "Fen: ", ""))
+            ElseIf InStr(ligne, "quality:") > 0 Then
+                chaine = chaine & ligne & vbCrLf
+            End If
+        End While
+
+        Return chaine
+    End Function
+
+    Public Function expV2(cheminEXP As String) As Boolean
+        Dim lecture As IO.FileStream, tampon As Long, tabTampon() As Byte
+
+        lecture = New IO.FileStream(cheminEXP, IO.FileMode.Open)
+
+        'SugaR Experience version 2
+        '0123456789abcdef0123456789
+        tampon = 26
+        ReDim tabTampon(tampon - 1)
+        lecture.Read(tabTampon, 0, tampon)
+        lecture.Close()
+
+        If System.Text.Encoding.UTF8.GetString(tabTampon) <> "SugaR Experience version 2" Then
+            Return False
+        Else
+            Return True
+        End If
+
+    End Function
+
+    Public Function gauche(texte As String, longueur As Integer) As String
+        If longueur > 0 Then
+            Return VB.Left(texte, longueur)
+        Else
+            Return ""
+        End If
+    End Function
+
+    Public Function listerCoupsLegaux(position As String) As String
+        Dim chaine As String, liste As String, tabChaine() As String
+
+        'on cherche tous les coups possibles
+        entree.WriteLine("setoption name MultiPV value " & maxMultiPVMoteur(moteur_court))
+
+        If position = "" Then
+            entree.WriteLine("position startpos")
+        ElseIf InStr(position, "/", CompareMethod.Text) > 0 _
+          And (InStr(position, " w ", CompareMethod.Text) > 0 Or InStr(position, " b ", CompareMethod.Text) > 0) Then
+            entree.WriteLine("position fen " & position)
+        Else
+            entree.WriteLine("position startpos moves " & position)
+        End If
+
+        entree.WriteLine("go depth 1")
+
+        chaine = ""
+        liste = ""
+        While InStr(chaine, "bestmove", CompareMethod.Text) = 0
+            chaine = sortie.ReadLine
+            If InStr(chaine, " pv ", CompareMethod.Text) > 0 Then
+                tabChaine = Split(chaine, " ")
+                For i = 0 To UBound(tabChaine) - 1
+                    If InStr(tabChaine(i), "pv", CompareMethod.Text) > 0 And tabChaine(i + 1) <> "" And Len(tabChaine(i + 1)) = 4 Then
+                        'info depth 0 seldepth 0 multipv 0 score cp 0 nodes 214 nps 71333 tbhits 369 time 3 pv d7d5
+                        '1 : d7d5, depth: 0, eval: cp 0, count: 0, quality: 0
+                        liste = liste & "1 : " & tabChaine(i + 1) & ", depth: 0, eval: cp 0, count: 0, quality: 0" & vbCrLf
+                    End If
+                Next
+            End If
+            Threading.Thread.Sleep(1)
+        End While
+        entree.WriteLine("stop")
+
+        entree.WriteLine("isready")
+        chaine = ""
+        While InStr(chaine, "readyok") = 0
+            chaine = sortie.ReadLine
+            Threading.Thread.Sleep(1)
+        End While
+
+        entree.WriteLine("setoption name MultiPV value 1")
+
+        entree.WriteLine("isready")
+        chaine = ""
+        While InStr(chaine, "readyok") = 0
+            chaine = sortie.ReadLine
+            Threading.Thread.Sleep(1)
+        End While
+
+        Return liste
+    End Function
+
+    Public Function maxMultiPVMoteur(chaine As String) As Integer
+        maxMultiPVMoteur = 200
+        If InStr(chaine, "asmfish", CompareMethod.Text) > 0 Then
+            maxMultiPVMoteur = 224
+        ElseIf InStr(chaine, "brainfish", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "brainlearn", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "stockfish", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "cfish", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "sugar", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "eman", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "hypnos", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "judas", CompareMethod.Text) > 0 _
+            Or InStr(chaine, "aurora", CompareMethod.Text) > 0 Then
+            maxMultiPVMoteur = 500
+        ElseIf InStr(chaine, "houdini", CompareMethod.Text) > 0 Then
+            maxMultiPVMoteur = 220
+        ElseIf InStr(chaine, "komodo", CompareMethod.Text) > 0 Then
+            maxMultiPVMoteur = 218
+        End If
+
+        Return maxMultiPVMoteur
+    End Function
+
+    Public Function nbCaracteres(ByVal chaine As String, ByVal critere As String) As Integer
+        Return Len(chaine) - Len(Replace(chaine, critere, ""))
+    End Function
+
+    Public Function nomFichier(chemin As String) As String
+        Return My.Computer.FileSystem.GetName(chemin)
+    End Function
+
+    Public Sub pgnUCI(chemin As String, fichier As String, suffixe As String, Optional priorite As Integer = 64)
+        Dim nom As String, commande As New Process()
+        Dim dossierFichier As String, dossierTravail As String
+
+        nom = Replace(nomFichier(fichier), ".pgn", "")
+
+        dossierFichier = fichier.Substring(0, fichier.LastIndexOf("\"))
+        dossierTravail = My.Computer.FileSystem.GetParentPath(chemin)
+
+        'si pgn-extract.exe ne se trouve à l'emplacement prévu (par <nom_ordinateur>.ini)
+        If Not My.Computer.FileSystem.FileExists(dossierTravail & "\pgn-extract.exe") Then
+
+            'si pgn-extract.exe ne se trouve dans le même dossier que le notre application
+            dossierTravail = Environment.CurrentDirectory
+            If Not My.Computer.FileSystem.FileExists(dossierTravail & "\pgn-extract.exe") Then
+
+                'on cherche s'il se trouve dans le même dossier que le fichierPGN
+                dossierTravail = dossierFichier
+                If Not My.Computer.FileSystem.FileExists(dossierTravail & "\pgn-extract.exe") Then
+
+                    'pgn-extract.exe est introuvable
+                    MsgBox("Veuillez copier pgn-extract.exe dans :" & vbCrLf & dossierTravail, MsgBoxStyle.Critical)
+                    dossierTravail = Environment.CurrentDirectory
+                    If Not My.Computer.FileSystem.FileExists(dossierTravail & "\pgn-extract.exe") Then
+                        End
+                    End If
+                End If
+            End If
+
+        End If
+
+        'si le fichierPGN ne se trouve pas dans le dossier de travail
+        If dossierFichier <> dossierTravail Then
+            'on recopie temporairement le fichierPGN dans le dossierTravail
+            My.Computer.FileSystem.CopyFile(fichier, dossierTravail & "\" & nom & ".pgn", True)
+        End If
+
+        commande.StartInfo.FileName = dossierTravail & "\pgn-extract.exe"
+        commande.StartInfo.WorkingDirectory = dossierTravail
+
+        If InStr(nom, " ") = 0 Then
+            commande.StartInfo.Arguments = " -s -Wuci -o" & nom & suffixe & ".pgn" & " " & nom & ".pgn"
+        Else
+            commande.StartInfo.Arguments = " -s -Wuci -o""" & nom & suffixe & ".pgn""" & " """ & nom & ".pgn"""
+        End If
+
+        commande.StartInfo.CreateNoWindow = True
+        commande.StartInfo.UseShellExecute = False
+        commande.Start()
+        commande.PriorityClass = priorite '64 (idle), 16384 (below normal), 32 (normal), 32768 (above normal), 128 (high), 256 (realtime)
+        commande.WaitForExit()
+
+        'si le dossierTravail ne correspond pas au dossier du fichierPGN
+        If dossierFichier <> dossierTravail Then
+            'on déplace le fichier moteur
+            Try
+                My.Computer.FileSystem.DeleteFile(dossierTravail & "\" & nom & ".pgn")
+            Catch ex As Exception
+
+            End Try
+            My.Computer.FileSystem.MoveFile(dossierTravail & "\" & nom & suffixe & ".pgn", dossierFichier & "\" & nom & suffixe & ".pgn")
+        End If
+
+    End Sub
+End Module
---
+++ expRate/modMain.vb
@@ -0,0 +1,335 @@
+﻿Module modMain
+    Public entete As String
+    Public epd As String
+    Public longueurMax As Integer
+    Public experienceRate As Integer
+    Public nbOuverturesTraitees As Integer
+    Public moteur_court As String
+
+    Sub Main()
+        Dim chaine As String, tabChaine() As String, tabTmp() As String, i As Integer
+        Dim fichierINI As String, fichierEXP As String, moteurEXP As String, fichierLOG As String
+        Dim modeReverse As Boolean, tabOuvertures() As String, indexOuvertures As Integer, nbOuvertures As Integer
+
+        Console.Title = My.Computer.Name
+
+        If My.Computer.FileSystem.GetFileInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) & "Documents\Visual Studio 2013\Projects\expRate\expRate\bin\Debug\expRate.exe").LastWriteTime > My.Computer.FileSystem.GetFileInfo(My.Application.Info.AssemblyName & ".exe").LastWriteTime Then
+            MsgBox("Il existe une version plus récente de ce programme !", MsgBoxStyle.Information)
+            End
+        End If
+
+        fichierINI = My.Computer.Name & ".ini"
+        moteurEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\20T Eman 8.20 x64 PCNT.exe"
+        fichierEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\Eman.exp"
+        If My.Computer.Name = "BOIS" Or My.Computer.Name = "HTPC" Or My.Computer.Name = "TOUR-COURTOISIE" Then
+            moteurEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\20T Eman 8.20 x64 BMI2.exe"
+            fichierEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\Eman.exp"
+        ElseIf My.Computer.Name = "BUREAU" Or My.Computer.Name = "WORKSTATION" Then
+            moteurEXP = "E:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\20T Eman 8.20 x64 BMI2.exe"
+            fichierEXP = "E:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\Eman.exp"
+        End If
+
+        If My.Computer.FileSystem.FileExists(fichierINI) Then
+            chaine = My.Computer.FileSystem.ReadAllText(fichierINI)
+            If chaine <> "" And InStr(chaine, vbCrLf) > 0 Then
+                tabChaine = Split(chaine, vbCrLf)
+                For i = 0 To UBound(tabChaine)
+                    If tabChaine(i) <> "" And InStr(tabChaine(i), " = ") > 0 Then
+                        tabTmp = Split(tabChaine(i), " = ")
+                        If tabTmp(0) <> "" And tabTmp(1) <> "" Then
+                            If InStr(tabTmp(1), "//") > 0 Then
+                                tabTmp(1) = Trim(gauche(tabTmp(1), tabTmp(1).IndexOf("//") - 1))
+                            End If
+                            Select Case tabTmp(0)
+                                Case "moteurEXP"
+                                    moteurEXP = Replace(tabTmp(1), """", "")
+                                Case "fichierEXP"
+                                    fichierEXP = Replace(tabTmp(1), """", "")
+                                Case Else
+
+                            End Select
+                        End If
+                    End If
+                Next
+            End If
+        End If
+        My.Computer.FileSystem.WriteAllText(fichierINI, "moteurEXP = " & moteurEXP & vbCrLf _
+                                                      & "fichierEXP = " & fichierEXP & vbCrLf, False)
+
+        If Not expV2(fichierEXP) Then
+            MsgBox(nomFichier(fichierEXP) & " <> experience format v2 !?", MsgBoxStyle.Exclamation)
+            End
+        End If
+
+        moteur_court = nomFichier(moteurEXP)
+
+        chaine = Replace(Command(), """", "")
+        If chaine = "" Then
+            Console.WriteLine("Which position ?")
+            Console.WriteLine("(enter an UCI string or leave blank for the default startpos)")
+            chaine = Trim(Console.ReadLine)
+        End If
+        
+        longueurMax = 0
+        modeReverse = False
+        If MsgBox("Normal (Yes) or Reverse (No) ?", MsgBoxStyle.YesNo) = MsgBoxResult.No Then
+            modeReverse = True
+        End If
+        If My.Computer.FileSystem.FileExists(chaine) Then
+            tabOuvertures = listerPositions(chaine)
+            nbOuvertures = tabOuvertures.Length - 1
+
+            fichierLOG = Replace(nomFichier(chaine), ".pgn", " (" & Replace(moteur_court, ".exe", "") & ").log")
+            If My.Computer.FileSystem.FileExists(fichierLOG) Then
+                My.Computer.FileSystem.DeleteFile(fichierLOG)
+            End If
+
+            Console.Write(vbCrLf & "Loading " & moteur_court & "... ")
+            chargerMoteur(moteurEXP, fichierEXP)
+            Console.WriteLine("OK" & vbCrLf)
+
+            experienceRate = 0
+            nbOuverturesTraitees = 0
+            For indexOuvertures = 1 To nbOuvertures
+                If tabOuvertures(indexOuvertures - 1) <> "" Then
+                    tabTmp = Split(tauxExperience(tabOuvertures(indexOuvertures - 1), modeReverse), ";")
+                    My.Computer.FileSystem.WriteAllText(fichierLOG, tabTmp(0) & StrDup(longueurMax - Len(tabTmp(0)), " ") & " : " & tabTmp(1) & vbCrLf, True)
+                    Console.Title = My.Computer.Name & " : " & "expRate @ " & Format(experienceRate / nbOuverturesTraitees, "0%") & ", opening @ " & Format(indexOuvertures / nbOuvertures, "0%")
+                End If
+            Next
+
+            My.Computer.FileSystem.WriteAllText(fichierLOG, vbCrLf & "Experience Rate : " & Format(experienceRate / nbOuverturesTraitees, "0%") & vbCrLf, True)
+
+            'décharger le moteur
+            dechargerMoteur()
+        Else
+            Console.Write(vbCrLf & "Loading " & moteur_court & "... ")
+            chargerMoteur(moteurEXP, fichierEXP)
+            Console.WriteLine("OK" & vbCrLf)
+
+            Console.WriteLine(entete)
+
+            Console.Title = "searching in " & nomFichier(fichierEXP)
+            tauxExperience(chaine, modeReverse)
+
+            'décharger le moteur
+            dechargerMoteur()
+        End If
+
+        Console.WriteLine("Press ENTER to close the window.")
+        Console.ReadLine()
+    End Sub
+
+    Private Function listerPositions(fichierPGN As String) As String()
+        Dim chaine As String, tabChaine() As String, i As Integer
+        Dim fichierUCI As String
+
+        fichierUCI = Replace(fichierPGN, ".pgn", "_uci.pgn")
+        If My.Computer.FileSystem.FileExists(fichierUCI) Then
+            My.Computer.FileSystem.DeleteFile(fichierUCI)
+        End If
+        pgnUCI("pgn-extract.exe", fichierPGN, "_uci")
+        tabChaine = Split(My.Computer.FileSystem.ReadAllText(fichierUCI), vbCrLf)
+
+        chaine = ""
+
+        For i = 0 To UBound(tabChaine)
+            If tabChaine(i) <> "" Then
+                If InStr(tabChaine(i), "[") = 0 And InStr(tabChaine(i), "]") = 0 Then
+                    tabChaine(i) = Trim(Replace(tabChaine(i), "*", ""))
+                    tabChaine(i) = Trim(Replace(tabChaine(i), "1-0", ""))
+                    tabChaine(i) = Trim(Replace(tabChaine(i), "0-1", ""))
+                    tabChaine(i) = Trim(Replace(tabChaine(i), "1/2-1/2", ""))
+                    chaine = chaine & Trim(tabChaine(i)) & vbCrLf
+                    If Len(Trim(tabChaine(i))) > longueurMax Then
+                        longueurMax = Len(Trim(tabChaine(i)))
+                    End If
+                End If
+            End If
+        Next
+
+        If My.Computer.FileSystem.FileExists(fichierUCI) Then
+            My.Computer.FileSystem.DeleteFile(fichierUCI)
+        End If
+
+        Return Split(chaine, vbCrLf)
+    End Function
+
+    Private Function tauxExperience(position As String, Optional modeReverse As Boolean = False) As String
+        Dim chaine As String, tabChaine() As String, tabTmp() As String, i As Integer, positionVide As Boolean
+        Dim departEPD As String, departEPDMem As String, coup As String, horizon As Integer
+        Dim tabPositions(1000000) As String, tabPositionsMem As String, indexPosition As Integer, nbCoupsVides As Integer, nbPositions As Integer
+        Dim total As Integer, totalMem As Integer, moyenne As Integer, moyenneMem As Integer, visite As Integer, profondeur As Integer, offsetPosition As Integer, nbPositionsMem As Integer
+
+        departEPDMem = ""
+        tabPositionsMem = ""
+        nbPositionsMem = 0
+        totalMem = 0
+        moyenneMem = 0
+
+mode_reverse:
+
+        horizon = 80 'nombre de coups maxi
+
+        tabPositions(0) = position
+        indexPosition = 0
+
+        departEPD = ""
+
+        positionVide = False
+
+        visite = 0
+        total = 0
+
+        profondeur = 0
+        moyenne = 0
+
+        offsetPosition = 0
+        nbCoupsVides = 0
+        nbPositions = 0
+
+        Do
+            position = tabPositions(indexPosition)
+            chaine = expListe(position)
+            If indexPosition = 0 Then
+                If tabPositions(0) <> "" Then
+                    departEPD = epd
+                Else
+                    departEPD = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+                End If
+            End If
+
+            If chaine = "" And Not positionVide Then
+                chaine = listerCoupsLegaux(position)
+                positionVide = True
+                nbCoupsVides = 0
+            End If
+
+            If chaine <> "" Then
+                tabChaine = Split(chaine, vbCrLf)
+                For i = 0 To UBound(tabChaine)
+                    If tabChaine(i) <> "" And InStr(tabChaine(i), "mate", CompareMethod.Text) = 0 Then
+                        tabTmp = Split(Replace(tabChaine(i), ":", ","), ",")
+
+                        coup = Trim(tabTmp(1))
+                        chaine = Trim(position & " " & coup)
+
+                        visite = CInt(Trim(tabTmp(7)))
+                        profondeur = CInt(Trim(tabTmp(3)))
+
+                        If positionVide And profondeur = 0 And visite = 0 Then
+                            nbCoupsVides = nbCoupsVides + 1
+                        Else
+                            total = total + visite
+                            moyenne = moyenne + profondeur
+                        End If
+
+                        nbPositions = offsetPosition - nbCoupsVides
+
+                        If nbPositions < 1000000 Then
+                            offsetPosition = offsetPosition + 1
+                            If offsetPosition > UBound(tabPositions) Then
+                                ReDim Preserve tabPositions(offsetPosition * 1.1)
+                            End If
+                            tabPositions(offsetPosition) = chaine
+
+                            If nbPositions Mod 5000 = 0 Then
+                                Console.Clear()
+                                Console.WriteLine("start position  : " & departEPD & vbCrLf)
+                                If tabPositions(0) = "" Then
+                                    Console.WriteLine("uci move string : startpos" & vbCrLf)
+                                Else
+                                    Console.WriteLine("uci move string : " & tabPositions(0) & vbCrLf)
+                                End If
+                                If nbPositions = 0 Then
+                                    Console.WriteLine("Experience rate : 0 moves, avg. D00, 000 counts/move" & vbCrLf)
+                                Else
+                                    Console.WriteLine("Experience rate : " & Trim(Format(nbPositions, "# ### ##0")) & " moves, avg. D" & Format(moyenne / nbPositions, "00") & ", " & Format(total / nbPositions, "000") & " counts/move" & vbCrLf)
+                                End If
+                            End If
+                        End If
+                    End If
+
+                    'reverse ?
+                    If modeReverse Then
+                        If nbPositions >= 5000 Then
+                            indexPosition = indexPosition + 1
+                            Exit Do
+                        End If
+                    End If
+                Next
+            End If
+
+            indexPosition = indexPosition + 1
+        Loop While tabPositions(indexPosition) <> "" And nbCaracteres(tabPositions(indexPosition), " ") < (horizon - 1) And nbPositions < 1000000
+
+        'reverse ?
+        If modeReverse Then
+            If nbPositions < 1000 Then
+                If Len(tabPositions(0)) >= 9 Then
+                    'on sauve la position et les stats actuelles
+                    departEPDMem = departEPD
+                    tabPositionsMem = tabPositions(0)
+                    nbPositionsMem = nbPositions
+                    totalMem = total
+                    moyenneMem = moyenne
+
+                    position = gauche(tabPositions(0), Len(tabPositions(0)) - 5)
+                    Array.Clear(tabPositions, 0, tabPositions.Length)
+                    GoTo mode_reverse
+                End If
+            ElseIf tabPositionsMem <> "" Then
+                'on restaure la position et les stats précédentes
+                departEPD = departEPDMem
+                tabPositions(0) = tabPositionsMem
+                nbPositions = nbPositionsMem
+                total = totalMem
+                moyenne = moyenneMem
+            End If
+        End If
+
+        Console.Clear()
+        Console.WriteLine("start position  : " & departEPD & vbCrLf)
+        Console.WriteLine("uci move string : " & tabPositions(0) & vbCrLf)
+
+        chaine = "0 000 000 moves, avg. D00, 000 counts/move"
+        If nbPositions > 0 Then
+            chaine = Format(nbPositions, "0 000 000") & " moves, avg. D" & Format(moyenne / nbPositions, "00") & ", " & Format(total / nbPositions, "000") & " counts/move"
+        End If
+
+        nbOuverturesTraitees = nbOuverturesTraitees + 1
+        If nbPositions >= 1000 Then
+            experienceRate = experienceRate + 1
+        End If
+        If gauche(chaine, 8) = "0 000 00" Then
+            chaine = Replace(chaine, "0 000 00", "........", , 1)
+        End If
+        If gauche(chaine, 7) = "0 000 0" Then
+            chaine = Replace(chaine, "0 000 0", ".......", , 1)
+        End If
+        If gauche(chaine, 6) = "0 000 " Then
+            chaine = Replace(chaine, "0 000 ", "......", , 1)
+        End If
+        If gauche(chaine, 5) = "0 000" Then
+            chaine = Replace(chaine, "0 000", ".....", , 1)
+        End If
+        If gauche(chaine, 4) = "0 00" Then
+            chaine = Replace(chaine, "0 00", "....", , 1)
+        End If
+        If gauche(chaine, 3) = "0 0" Then
+            chaine = Replace(chaine, "0 0", "...", , 1)
+        End If
+        If gauche(chaine, 2) = "0 " Then
+            chaine = Replace(chaine, "0 ", "..", , 1)
+        End If
+        Console.WriteLine("Experience rate : " & Trim(chaine) & vbCrLf)
+
+        chaine = tabPositions(0) & ";" & chaine
+
+        tabPositions = Nothing
+
+        Return chaine
+
+    End Function
+
+End Module
